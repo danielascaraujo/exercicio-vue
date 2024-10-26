a9c0c0355dd3a9482ba7f450c5884c0a88677c4e
Daniela Araujo <daniela.scaraujo@outlook.com>
2024-10-26 16:49:04 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: '+',
+    };
+  },
+  computed: {
+    resultado() {
+      switch (this.operacao) {
+        case '+':
+          return this.numero1 + this.numero2;
+        case '-':
+          return this.numero1 - this.numero2;
+        case '*':
+          return this.numero1 * this.numero2;
+        case '/':
+          return this.numero1 / this.numero2;
+        default:
+          return 0;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <input type="number" v-model.number="numero1" />
+      <select v-model="operacao">
+        <option value="+">Soma</option>
+        <option value="-">Subtração</option>
+        <option value="*">Multiplicação</option>
+        <option value="/">Divisão</option>
+      </select>
+    <input type="number" v-model.number="numero2" />
+    <p>Resultado: {{ resultado }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
